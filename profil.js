31ebb185a1890a09d1ca5b62987f9e99636376ba
Jerebtx <126483477+Jerebtx@users.noreply.github.com>
2024-01-12 21:10:41 +0700

    profil.js

---
+++ profil.js
@@ -0,0 +1,12 @@
+
+document.addEventListener('DOMContentLoaded', function () {
+    
+});
+
+function redirectToEditProfil() {
+    window.location.href = './editprofil.html';
+}
+
+function redirectToBeranda() {
+    window.location.href = './beranda.html';
+}
